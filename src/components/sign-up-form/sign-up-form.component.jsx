ed7f25f0c63ba3df1e207dcbe9bf6b19464c7cf7
Vic Baker <vic.baker@matricinnovates.com>
2023-07-17 14:00:21 -0400

    completion of 102

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,8 @@
 // 98
 import { useState } from 'react'
 import { FormInput } from '../form-input/form-input.component'
+import './sign-up-form.styles.scss'
+import { Button } from '../button/button.component'
 
 import { auth, createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils' // 99
 
@@ -54,14 +56,15 @@ const SignUpForm = () => {
     }
 
     return (
-        <div>
-            <h1>Sign up with your email and password</h1>
+        <div className='sign-up-container'>
+            <h2>Don't have an account?</h2>
+            <span>Sign up with your email and password</span>
             <form onSubmit={handleSubmit}>
                 <FormInput label="Display Name" type="text" required onChange={handleChange} name='displayName' value={displayName}/>
                 <FormInput label="Email" type="email" required onChange={handleChange} name='email' value={email}/>
                 <FormInput label="Password" type="password" required onChange={handleChange} name='password' value={password}/>
                 <FormInput label="Confirm Password" type="password" required onChange={handleChange} name='confirmPassword' value={confirmPassword}/>
-                <button type="submit">Sign Up</button>
+                <Button type="submit">Sign Up</Button>
 
             </form>
         </div>
