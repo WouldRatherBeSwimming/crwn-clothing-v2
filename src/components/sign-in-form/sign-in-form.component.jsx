1a5b562524183816d7130f7d687ce4eb1a5be2e1
Vic Baker <vic.baker@matricinnovates.com>
2023-07-18 09:28:46 -0400

    fixed style and Sign In button now works 106 still in progress.  I wanted to fix the button functionality.

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -22,7 +22,7 @@ const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields)
     const { email, password } = formFields
 
-    console.log(formFields)
+    // console.log(formFields)
 
     const {setCurrentUser} = useContext(UserContext)
 
@@ -39,6 +39,7 @@ const SignInForm = () => {
     // 99
     const handleSubmit = async (event) => {
         event.preventDefault();
+        // console.log(event)
 
         try {
             const {user} = await signInAuthUserWithEmailAndPassword(email, password)
@@ -66,14 +67,14 @@ const SignInForm = () => {
     }
 
     return (
-        <div className='sign-up-container'>
+        <div className='sign-in-container'>
             <h2>Already have an account?</h2>
             <span>Sign in with your email and password</span>
             <form onSubmit={handleSubmit}>
                 <FormInput label="Email" type="email" required onChange={handleChange} name='email' value={email}/>
                 <FormInput label="Password" type="password" required onChange={handleChange} name='password' value={password}/>
                 <div className='buttons-container'>
-                  <Button type="button">Sign In</Button>
+                  <Button type="submit">Sign In</Button>
                   <Button buttonType="google" onClick={signInWithGoogle}>Google sign in</Button>
                 </div>
             </form>
