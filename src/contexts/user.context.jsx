edc72d41b694c557d5fc6ca2ed904d10a35bc0ac
Vic Baker <vic.baker@matricinnovates.com>
2023-07-18 08:38:54 -0400

    106 in progress

---
+++ src/contexts/user.context.jsx
@@ -0,0 +1,14 @@
+// TODO: goto index.js and wrap the app
+import { createContext, useState } from "react"; 
+
+export const UserContext = createContext({
+    currentUser: null,
+    setCurrentUser: () => null
+
+})
+
+export const UserProvider = ({ children}) => {
+    const [currentUser, setCurrentUser] = useState(null)
+    const value = {currentUser, setCurrentUser}
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+}
